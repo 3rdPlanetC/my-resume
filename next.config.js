cbb335034f6e3b75ecb9869cec073e4aefa341bf
Eth Chonlasai <o_nongearth_o@hotmail.com>
2020-07-03 03:21:31 +0700

    test pm2 on heroku

--- next.config.js
+++ next.config.js
@@ -5,24 +5,21 @@ module.exports = withFonts(withLess({
         importLoaders: 1,
         localIdentName: "[local]___[hash:base64:5]",
     },
-    // lessLoaderOptions: {
-    //     javascriptEnabled: true,
-    // },
-    // webpack: config => {
-    //     config.module.rules.push({
-    //         test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-    //         use: {
-    //             loader: 'url-loader',
-    //             options: {
-    //                 limit: 100000,
-    //                 name: '[name].[ext]'
-    //             }
-    //         }
-    //     }),
-    //     config.module.rules.push({
-    //         test: /\.css$/i,
+    webpack: config => {
+        config.module.rules.push({
+            test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 100000,
+                    name: '[name].[ext]'
+                }
+            }
+        }),
+        config.module.rules.push({
+            test: /\.css$/i,
 
-    //     })
-    //     return config
-    // }
+        })
+        return config
+    }
 }))
