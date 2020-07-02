f1ed373045a3802de2f0ed501381f562def8cae1
Eth Chonlasai <o_nongearth_o@hotmail.com>
2020-07-03 01:36:22 +0700

    test pm2 on heroku

--- next.config.js
+++ next.config.js
@@ -6,4 +6,17 @@ module.exports = withFonts(withLess(withCSS({
         importLoaders: 1,
         localIdentName: "[local]___[hash:base64:5]",
     },
+    webpack: function (config) {
+        config.module.rules.push({
+            test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 100000,
+                    name: '[name].[ext]'
+                }
+            }
+        })
+        return config
+    }
 })))
