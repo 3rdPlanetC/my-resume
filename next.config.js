1a6ca074a58bdbdcb530a2f16509f6dd4509d7cc
Eth Chonlasai <o_nongearth_o@hotmail.com>
2020-07-03 02:00:21 +0700

    test pm2 on heroku

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@ const withLess = require('@zeit/next-less')
 const withFonts = require('next-fonts')
 const withCSS = require('@zeit/next-css')
 module.exports = withFonts(withCSS(withLess({
-    cssLoaderOptions: {
-        importLoaders: 1,
-        localIdentName: "[local]___[hash:base64:5]",
-    },
+    // cssLoaderOptions: {
+    //     importLoaders: 1,
+    //     localIdentName: "[local]___[hash:base64:5]",
+    // },
+    cssModules: true
     // webpack: function (config) {
     //     config.module.rules.push({
     //         test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
