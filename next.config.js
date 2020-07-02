bd6d84e60151ebb7840a9ef485550b6996947700
Eth Chonlasai <o_nongearth_o@hotmail.com>
2020-07-03 03:07:30 +0700

    test pm2 on heroku

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 const withLess = require('@zeit/next-less')
 const withFonts = require('next-fonts')
 module.exports = withFonts(withLess({
-    cssLoaderOptions: {
-        importLoaders: 1,
-        localIdentName: "[local]___[hash:base64:5]",
-    },
     lessLoaderOptions: {
         javascriptEnabled: true,
     },
