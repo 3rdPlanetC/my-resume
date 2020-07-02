bd7554cabfb72e9a6b1e0a7dff368578bfabdf2e
Eth Chonlasai <o_nongearth_o@hotmail.com>
2020-07-03 03:16:19 +0700

    test pm2 on heroku

--- next.config.js
+++ next.config.js
@@ -1,21 +1,28 @@
 const withLess = require('@zeit/next-less')
 const withFonts = require('next-fonts')
 module.exports = withFonts(withLess({
-    webpack: config => {
-        config.module.rules.push({
-            test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-            use: {
-                loader: 'url-loader',
-                options: {
-                    limit: 100000,
-                    name: '[name].[ext]'
-                }
-            }
-        }),
-        config.module.rules.push({
-            test: /\.css$/i,
+    cssLoaderOptions: {
+        importLoaders: 1,
+        localIdentName: "[local]___[hash:base64:5]",
+    },
+    // lessLoaderOptions: {
+    //     javascriptEnabled: true,
+    // },
+    // webpack: config => {
+    //     config.module.rules.push({
+    //         test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+    //         use: {
+    //             loader: 'url-loader',
+    //             options: {
+    //                 limit: 100000,
+    //                 name: '[name].[ext]'
+    //             }
+    //         }
+    //     }),
+    //     config.module.rules.push({
+    //         test: /\.css$/i,
 
-        })
-        return config
-    }
+    //     })
+    //     return config
+    // }
 }))
