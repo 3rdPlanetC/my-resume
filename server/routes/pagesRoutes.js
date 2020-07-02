f06a6d920c4ae9dda79d062b3d97ea2d62828017
Eth Chonlasai <o_nongearth_o@hotmail.com>
2020-07-02 12:29:37 +0700

    test backend

--- server/routes/pagesRoutes.js
+++ server/routes/pagesRoutes.js
@@ -1,25 +1,25 @@
 const { loginMiddleware } = require('./middleware')
 
 module.exports = (server, app) => {
-    server.get('/', (req, res) => {
-        return app.render(req, res, '/')
-    })
-    server.get('/post/:slug', (req, res) => {
-        return app.render(req, res, '/post', { slug: req.params.slug })
-    })
-    server.get('/blog', (req, res) => {
-        return app.render(req, res, '/blog')
-    })
-    server.get('/blog/:id', (req, res) => {
-        return app.render(req, res, '/blog/[id]')
-    })
-    server.get('/dashboard', loginMiddleware, (req, res) => {
-        return app.render(req, res, '/dashboard')
-    })
-    server.get('/login', (req, res) => {
-        return app.render(req, res, '/login')
-    })
-    server.get('/signup', (req, res) => {
-        return app.render(req, res, '/signup')
-    })
+    // server.get('/', (req, res) => {
+    //     return app.render(req, res, '/')
+    // })
+    // server.get('/post/:slug', (req, res) => {
+    //     return app.render(req, res, '/post', { slug: req.params.slug })
+    // })
+    // server.get('/blog', (req, res) => {
+    //     return app.render(req, res, '/blog')
+    // })
+    // server.get('/blog/:id', (req, res) => {
+    //     return app.render(req, res, '/blog/[id]')
+    // })
+    // server.get('/dashboard', loginMiddleware, (req, res) => {
+    //     return app.render(req, res, '/dashboard')
+    // })
+    // server.get('/login', (req, res) => {
+    //     return app.render(req, res, '/login')
+    // })
+    // server.get('/signup', (req, res) => {
+    //     return app.render(req, res, '/signup')
+    // })
 }
